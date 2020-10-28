c7b2b9bb816eb3a6f5d7af357850f859d9b4c8d9
Leandro Udala <leandroudala@gmail.com>
2020-10-28 13:19:50 -0300

    3rd class finished. Gateway and Eureka (Service Discovery) configured

--- gateway/src/main/java/app/udala/devdojo/GatewayApplication.java
+++ gateway/src/main/java/app/udala/devdojo/GatewayApplication.java
@@ -2,10 +2,11 @@ package app.udala.devdojo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableZuulProxy
 @EnableEurekaClient
 public class GatewayApplication {
